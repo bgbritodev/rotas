7bbc54fa894291ab85a7c3553aa0ed39001a3b0d
bgbritodev <bgbritodev@gmail.com>
2023-04-17 17:54:37 -0300

    added a api to use the data of th posts

--- estudos-1/src/App.js
+++ estudos-1/src/App.js
@@ -1,8 +1,37 @@
+// api link
+// sujeitoprogramador.com/rn-api/?api=posts
+import React, { useEffect, useState } from 'react';
+
+
+
 
 
 function App() {
+  const [nutri, setNutri] = useState([]);
+
+useEffect(()=>{
+
+  function loadApi(){
+    const url = 'https://sujeitoprogramador.com/rn-api/?api=posts'
+    //fetch é uma função base do javascript que faz requisição
+    fetch(url)
+    //fetch é uma promisse, entao devolve um .then caso sucesso
+    .then((r)=> r.json())
+    .then((data) => {
+      console.log(data)
+      setNutri(data)
+    })
+  }
+
+  loadApi();
+
+},[]);
+
   return (
-    <div className="App">
+    <div className="container" >
+      <header>
+        <strong>Nutri</strong>
+      </header>
     </div>
   );
 }
